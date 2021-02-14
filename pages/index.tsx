a0cf01bef00a086b2b5dd760710c5b8575d4554d
Pedro Kohler <pedrokohlerbh@gmail.com>
2021-02-14 16:21:13 -0300

    fix: estrutura tela cheia, nomes

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,23 @@
 import Head from 'next/head';
-import CarCard from '../src/components/Home/CarCard/CarCard';
-import createCarCardProps from '../src/components/Home/CarCard/helpers/factory';
-import { Container } from '../src/components/Home/Container';
-import Header from '../src/components/Home/Header/Header';
-import SectionHeader from '../src/components/Home/SectionHeader/SectionHeader';
+import CarCard from '../src/components/home/CarCard/CarCard';
+import createCarCardProps from '../src/components/home/CarCard/helpers/factory';
+import { Container, CarListContainer } from '../src/components/home/Container';
+import Header from '../src/components/home/Header/Header';
+import SectionHeader from '../src/components/home/SectionHeader/SectionHeader';
 import Menu from '../src/components/Menu/Menu';
 
 // @todo: load cars
 const cars = [
-  createCarCardProps(),
-  createCarCardProps(),
-  createCarCardProps(),
+  createCarCardProps({ car: 'Carro' }),
+  createCarCardProps({ car: 'Carro1' }),
+  createCarCardProps({ car: 'Carro2' }),
+  createCarCardProps({ car: 'Carro3' }),
+  createCarCardProps({ car: 'Carro4' }),
+  createCarCardProps({ car: 'Carro5' }),
+  createCarCardProps({ car: 'Carro6' }),
+  createCarCardProps({ car: 'Carro7' }),
+  createCarCardProps({ car: 'Carro8' }),
+  createCarCardProps({ car: 'Carro9' }),
 ];
 
 export default function Home() {
@@ -23,7 +30,9 @@ export default function Home() {
         <Header />
         <section>
           <SectionHeader />
-          {cars.map((car) => <CarCard {...car} />)}
+          <CarListContainer>
+            {cars.map((car) => <CarCard key={car.car} {...car} />)}
+          </CarListContainer>
           <Menu />
         </section>
       </Container>
---
+++ src/components/home/Container.tsx
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`;
+
+export const CarListContainer = styled.div`
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    justify-content: space-evenly;
+`;
