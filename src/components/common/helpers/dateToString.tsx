047d65ef2c66956e31e4431986de9bd5990ff912
Pedro Kohler <pedrokohlerbh@gmail.com>
2021-02-15 09:20:04 -0300

    feat: criar status de locação

---
+++ src/components/common/helpers/dateToString.tsx
@@ -0,0 +1,8 @@
+const dateToString = (date): Date => date
+  .toLocaleDateString(
+    'pt-Br',
+    { year: 'numeric', month: 'long', day: 'numeric' },
+  )
+  .replace(/ de/g, '');
+
+export default dateToString;
