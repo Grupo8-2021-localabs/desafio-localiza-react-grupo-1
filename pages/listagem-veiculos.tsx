6317e764acefafd7e89252a24921cff39c192ac5
Ermelinda <ermelindaabreu@ioasys.com.br>
2021-02-16 19:58:19 -0300

    feat: construindo layout da pagina de listagem

---
+++ pages/listagem-veiculos.tsx
@@ -0,0 +1,42 @@
+import Head from 'next/head';
+import CarCard from '../src/components/common/CarCard/CarCard';
+import createCarCardProps from '../src/components/home/CarCard/helpers/factory';
+import { Container, CarListContainer } from '../src/components/home/Container';
+import SectionHeader from '../src/components/home/SectionHeader/SectionHeader';
+import Menu from '../src/components/Menu/Menu';
+import Header from '../src/components/common/Header/Header';
+
+const cars = [
+  createCarCardProps({ car: 'Carro' }),
+  createCarCardProps({ car: 'Carro1' }),
+  createCarCardProps({ car: 'Carro2' }),
+  createCarCardProps({ car: 'Carro3' }),
+  createCarCardProps({ car: 'Carro4' }),
+  createCarCardProps({ car: 'Carro5' }),
+  createCarCardProps({ car: 'Carro6' }),
+  createCarCardProps({ car: 'Carro7' }),
+  createCarCardProps({ car: 'Carro8' }),
+  createCarCardProps({ car: 'Carro9' }),
+];
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Desafio Localiza</title>
+      </Head>
+      <Container>
+        <Header title="Listagem" description="12 carros" />
+        <section>
+          <SectionHeader />
+          <CarListContainer>
+            {cars.map((car) => (
+              <CarCard key={car.car} {...car} />
+            ))}
+          </CarListContainer>
+          <Menu />
+        </section>
+      </Container>
+    </>
+  );
+}
