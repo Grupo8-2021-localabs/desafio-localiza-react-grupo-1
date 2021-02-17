aacee43fb51bd9e2cfb7f2ecb8b313e836fa8cda
Ermelinda <ermelindaabreu@ioasys.com.br>
2021-02-17 20:43:47 -0300

    fix: aplicando correçoes iniciais formulario de cadastro

--- src/templates/Register/Register.tsx
+++ src/templates/Register/Register.tsx
@@ -1,24 +1,25 @@
+import { FC, useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { InferType } from 'yup';
 import { yupResolver } from '@hookform/resolvers/yup';
-import { useRouter } from 'next/router'
+import { useRouter } from 'next/router';
 import Input from '../../components/Input/Input';
 import Button from '../../components/Button/Button';
 
 import CpfIcon from '../../components/icons/CpfIcon/CpfIcon';
 import PasswordIcon from '../../components/icons/PasswordIcon/PasswordIcon';
-import PersonIcon from '../../components/icons/PersonIcon/index'
-import GoBackIcon from '../../components/icons/GoBackIcon/index'
+import PersonIcon from '../../components/icons/PersonIcon/index';
+import GoBackIcon from '../../components/icons/GoBackIcon/index';
 import { Container, ContentTitle, SubTitle } from '../Login/styled-components/index';
-import { schema } from '../Login/Login.validations';
-import { useState } from 'react';
-import {RegisterContainer, GoBack,ContainerCircle, Circle} from './Register.styled'
+import { defaultValues, schema } from './Register.validations';
+import { RegisterContainer, GoBack, ContainerCircle, Circle } from './Register.styled';
 
 type SignUp = InferType<typeof schema>;
 
-const Register = () => {
+const Register: FC = () => {
   const { register, handleSubmit, errors } = useForm({
     resolver: yupResolver(schema),
+    defaultValues,
   });
   const onSubmit = (data: SignUp) => console.log(data);
 
@@ -29,13 +30,13 @@ const Register = () => {
   return (
     <Container>
       <RegisterContainer>
-      <GoBack onClick={goBack}>
-      <GoBackIcon color={'#7A7A80'}/>
-      </GoBack>
-      <ContainerCircle>
-      <Circle />
-      <Circle /> 
-      </ContainerCircle>
+        <GoBack onClick={goBack}>
+          <GoBackIcon color="#7A7A80" />
+        </GoBack>
+        <ContainerCircle>
+          <Circle />
+          <Circle />
+        </ContainerCircle>
       </RegisterContainer>
       <ContentTitle>
         <h1>
@@ -44,84 +45,82 @@ const Register = () => {
         </h1>
         <p>Faça seu cadastro de forma rápida e fácil.</p>
       </ContentTitle>
-      {!isSecondPart ? 
-    <>
-      <SubTitle>
-        <h2>
-          01. Dados
-        </h2>
-      </SubTitle>
-      <form onSubmit={handleSubmit(onSubmit)}>
-      <Input
-          type="text"
-          name="name"
-          placeholder="Nome"
-          register={register}
-          iconLeft={<PersonIcon color={'#7A7A80'}/>}
-          className="input-style"
-        />
+      {!isSecondPart ? (
+        <>
+          <SubTitle>
+            <h2>01. Dados</h2>
+          </SubTitle>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <Input
+              type="text"
+              name="name"
+              placeholder="Nome"
+              register={register}
+              iconLeft={<PersonIcon color="#7A7A80" />}
+              className="input-style"
+            />
 
-        <Input
-          type="cpf"
-          name="cpf"
-          placeholder="CPF"
-          register={register}
-          error={errors?.cpf?.message}
-          iconLeft={<CpfIcon />}
-          className="input-style"
-        />    
+            <Input
+              type="cpf"
+              name="cpf"
+              placeholder="CPF"
+              register={register}
+              maxLength={11}
+              error={errors?.cpf?.message}
+              iconLeft={<CpfIcon />}
+              className="input-style"
+            />
 
-        <div className="button-container">
-          <Button className="button-style" onClick = { () => setisSecondPart(true)} type={'submit'}>
-            Próximo 
-          </Button>
-        </div>
-      </form>
-    </>
-    : 
-    <>
-    <SubTitle>
-      <h2>
-        02. Senha
-      </h2>
-    </SubTitle>
-    <form onSubmit={handleSubmit(onSubmit)}>
-        <Input
-          type="password"
-          name="password"
-          placeholder="Senha"
-          register={register}
-          error={errors?.password?.message}
-          iconLeft={<PasswordIcon />}
-          className="input-style"
-        />
-        <Input
-          type="password"
-          name="password"
-          placeholder="Senha"
-          register={register}
-          error={errors?.password?.message}
-          iconLeft={<PasswordIcon />}
-          className="input-style"
-        />
+            <div className="button-container">
+              <Button className="button-style" onClick={() => setisSecondPart(true)} type="submit">
+                Próximo
+              </Button>
+            </div>
+          </form>
+        </>
+      ) : (
+        <>
+          <SubTitle>
+            <h2>02. Senha</h2>
+          </SubTitle>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <Input
+              type="password"
+              name="password"
+              placeholder="Senha"
+              register={register}
+              error={errors?.password?.message}
+              iconLeft={<PasswordIcon />}
+              className="input-style"
+            />
+            <Input
+              type="password"
+              name="passwordConfirmation"
+              placeholder="Senha"
+              register={register}
+              error={errors?.passwordConfirmation?.message}
+              iconLeft={<PasswordIcon />}
+              className="input-style"
+            />
 
-      <div className="button-container">
-        <Button className="button-style" type="submit" 
-           onClick={() => {
-              console.log("Cadastro Criado com sucesso");
-              setTimeout(() => {
-                router.push("/");
-              }, 2000);
-            }}>
-          Cadastrar 
-        </Button>
-      </div>
-    </form>
-  </>
-    }
-    
+            <div className="button-container">
+              <Button
+                className="button-style"
+                type="submit"
+                onClick={() => {
+                  console.log('Cadastro Criado com sucesso');
+                  setTimeout(() => {
+                    router.push('/');
+                  }, 2000);
+                }}
+              >
+                Cadastrar
+              </Button>
+            </div>
+          </form>
+        </>
+      )}
     </Container>
-    
   );
 };
 export default Register;
