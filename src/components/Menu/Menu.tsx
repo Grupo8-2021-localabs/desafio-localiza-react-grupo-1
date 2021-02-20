02fdbff66b632676657a257dd0931aca7b7c60cc
Pedro Kohler <pedrokohlerbh@gmail.com>
2021-02-20 03:37:00 -0300

    feat: fazer menu funcional

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -1,16 +1,41 @@
-import { FC } from 'react';
+import { FC, useEffect, useState } from 'react';
+import Link from 'next/link'
 import { IProps } from './types';
 import { Container } from './styled-components';
 import CalendarIcon from '../icons/CalendarIcon';
-import CarIcon from '../icons/CarIcon';
 import HomeIcon from '../icons/HomeIcon';
+import { useAuth } from '../AuthProvider';
 
-const Menu: FC<IProps> = () => (
-  <Container>
-    <HomeIcon color="#3D3D4D" />
-    <CarIcon color="#3D3D4D" />
-    <CalendarIcon color="#3D3D4D" />
-  </Container>
-);
+
+const Menu: FC<IProps> = ({ selected }) => {
+  const { authState } = useAuth();
+  const [icons, setIcons] = useState([]);
+
+  useEffect(() => {
+    const urlEncodedToken = encodeURIComponent(authState.token);
+    setIcons([
+      [HomeIcon, '/'],
+      [CalendarIcon, `/agendamentos/${authState.userCpf}?token=${urlEncodedToken}`],
+    ]);
+  }, [authState])
+
+  return (
+    <Container>
+      {icons.map(([Icon, path], i) => {
+      if(selected === i){
+        return <Icon key={path} color="#DC1637" />
+      }
+
+      return (
+        <Link key={path} href={path}>
+          <a href={path}>
+            <Icon color={selected !== i ? "#3D3D4D" : "#DC1637"} />
+          </a>
+        </Link>
+      )
+
+    })}
+    </Container>
+)};
 
 export default Menu;
